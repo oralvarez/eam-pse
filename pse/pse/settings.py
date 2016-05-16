c41383f630213d6c902f225af2f1c5810456b44f
Oscar Alvarez <oralvarez@gmail.com>
2016-05-16 05:36:16 -0500

    Agrregando filtros mejorados 2

--- pse/pse/settings.py
+++ pse/pse/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework.filters',
     'quickstart',
     'transacciones',
 )
